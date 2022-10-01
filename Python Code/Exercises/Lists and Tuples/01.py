ccd6fc4701b71d9756ab615e4d0f958418bab310
armycrih <armycrih@gmail.com>
2022-10-01 10:29:02 -0300

    Lists and Tuples created

---
+++ Python Code/Exercises/Lists and Tuples/01.py
@@ -0,0 +1,8 @@
+def show_subjects(subjects):
+    print("These are the subjects: ")
+    print(subjects)
+
+
+if __name__ == "__main__":
+    subjects = ["Matemáticas", "Física", "Química", "Historia", "Lengua"]
+    show_subjects(subjects)
